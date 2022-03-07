314ce663ca6b6750ffcc4869ec3d42c6541fa7ee
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-07 15:46:42 +0100

    Renderer2 to change style Angular Style !

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -1,12 +1,12 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective {
 
-  constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    //this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
-
 }
