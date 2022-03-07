457fb77709892756c6f76e2675d21d8ffbf76c31
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-07 16:08:30 +0100

    Boolean coercion

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -1,7 +1,13 @@
-import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appBasicHighlight]'
+  selector: '[appBasicHighlight]',
 })
 export class BasicHighlightDirective implements OnInit {
   private defaultHighlightColor = 'green';
@@ -9,19 +15,36 @@ export class BasicHighlightDirective implements OnInit {
   @Input('appBasicHighlight') highlightColor!: string;
   @HostBinding('style.backgroundColor') backgroundColor!: string;
 
+  private _disabled: boolean = false;
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+
+  set disabled(disabled: any) {
+    this._disabled = this.coerceBooleanProperty(disabled);
+  }
 
   constructor() {}
 
   ngOnInit(): void {
-    if (this.highlightColor === '') this.highlightColor = this.defaultHighlightColor;
+    if (this.highlightColor === '')
+      this.highlightColor = this.defaultHighlightColor;
     this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter() {
+    if (this._disabled) return;
     this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave() {
+    if (this._disabled) return;
     this.backgroundColor = this.defaultColor;
   }
+
+  coerceBooleanProperty = (value: any): boolean => {
+    return value != null && `${value}` !== 'false';
+  };
 }
