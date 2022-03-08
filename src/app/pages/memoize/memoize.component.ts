46cef1657d8f662d49320975e74b3fee86a99fcb
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-08 10:47:17 +0100

    Added memoize

--- src/app/pages/memoize/memoize.component.ts
+++ src/app/pages/memoize/memoize.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+import * as memoizee from 'memoizee';
+
+function add(a: number, b: number): number {
+  console.log('add is called');
+  return a + b;
+}
+
+const memoizedAdd = memoizee(add);
+
+memoizedAdd(1, 2); // log "add is called"
+memoizedAdd(1, 2); // cache hit, not logging
+memoizedAdd(1, 3); // log "add is called"
+
 @Component({
   selector: 'app-memoize',
   templateUrl: './memoize.component.html',
