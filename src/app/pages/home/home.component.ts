3e6709a94e9b4b2c9b822503e17a00ee5b88a52a
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-03-13 12:40:41 +0100

    Using lodash

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  movies = [
+    { name: 'Batman Begins', rating: 8.2 },
+    { name: 'Insomnia', rating: 7.1 },
+    { name: 'Doodlebug', rating: 7.1 },
+    { name: 'Test', rating: 2 },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    const movieExist = !!_.find(this.movies, { name: 'Batman Begins' });
+    console.log(movieExist);
   }
 
+  ngOnInit(): void {}
 }
